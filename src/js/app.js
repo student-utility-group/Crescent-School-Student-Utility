1a8d284cfde2b49e334881c694e05a6c2131028c
Libbux <j@thelibbster.com>
2013-11-13 15:23:44 -0500

    Add main logic file
    Add the main application logic file app.js.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
+function onDeviceReady() {
+    if (parseFloat(window.device.version) === 7.0) {
+          document.body.style.marginTop = "20px";
+    }
 	$('#login-button').click(function(){
 		$username = "hi there";
 		var store = new Lawnchair(function(){
 			this.save({'username': username});
-			this.get('username', function(u){alert(u);});
+			this.get('username', function(u){alert(u);
 		});
 	});
 });
+
+} // End of onDeviceReady
+
+document.addEventListener('deviceready', onDeviceReady, false);
