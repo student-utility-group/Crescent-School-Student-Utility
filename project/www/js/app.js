89bda7365911bf684ae012816ddb63915d89501b
Libbux <j@thelibbster.com>
2013-12-03 00:02:41 -0500

    Update app.js
    Trying to move all JS to app.js, where it belongs. Having some weird
    problem again whereby app.js doesn’t get run.

--- project/www/js/app.js
+++ project/www/js/app.js
@@ -1,16 +1,30 @@
 function onDeviceReady() {
+    
+    // Attach the FastClick library to the .click event
+    // FastClick.js
+    // https://github.com/ftlabs/fastclick
+    $(function () {
+        FastClick.attach(document.body);
+    });
+    
+    // iOS 7 status bars are translucent, we need to make our
+    // own margin at the top of the screen to prevent overlap.
     if (parseFloat(window.device.version) === 7.0) {
-          document.body.style.marginTop = "20px";
-    }
-	$('#login-button').click(function(){
-		$username = "hi there";
-		var store = new Lawnchair(function(){
-			this.save({'username': username});
-			this.get('username', function(u){alert(u);
-		});
-	});
-});
+        // The actual status bar is 20px, need to decide if
+        // we want more white space
+        document.body.style.marginTop = "20px";
+    }    
+    
+    // Main login-page event handling for the login form submission
+    $('#login-button').click(function () {
+        $('.login-page').slideUp();
+        $('.main-page').slideDown();
+    });
+
+    
 
 } // End of onDeviceReady
 
 document.addEventListener('deviceready', onDeviceReady, false);
+
+    
