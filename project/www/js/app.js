03c57083bc513e19812e763bec247bbd68990c60
Libbux <j@thelibbster.com>
2013-12-03 23:46:05 -0500

    Squash bug #17

--- project/www/js/app.js
+++ project/www/js/app.js
@@ -1,19 +1,12 @@
-function onDeviceReady() {
-    $(document).ready(function(){
+$(document).ready(function(){
         // Attach the FastClick library to the .click event
         // FastClick.js
         // https://github.com/ftlabs/fastclick
         $(function () {
             FastClick.attach(document.body);
         });
-        
-        // iOS 7 status bars are translucent, we need to make our
-        // own margin at the top of the screen to prevent overlap.
-        if (parseFloat(window.device.version) === 7.0) {
-            // The actual status bar is 20px, need to decide if
-            // we want more white space
-            document.body.style.marginTop = "20px";
-        }    
+
+         
         
         // Main login-page event handling for the login form submission
 //        $('#login-button').click(function () {
@@ -38,11 +31,3 @@ function onDeviceReady() {
         });
     
     });
-
-    
-
-} // End of onDeviceReady
-
-document.addEventListener('deviceready', onDeviceReady, false);
-
-    
