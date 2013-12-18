8c74c930d68d284bec651ad7f712b528e4f42a77
Libbux <j@thelibbster.com>
2013-12-17 22:08:05 -0500

    [20411] Clock functionality added

--- project/www/js/app.js
+++ project/www/js/app.js
@@ -30,4 +30,43 @@ $(document).ready(function(){
             $('.login-page').slideDown();
         });
     
-    });
+        function myDate(){
+            
+            var now = new Date();
+            var outHour = now.getHours();
+            
+            if(outHour >12) {
+                var ampm = "PM";
+                newHour = outHour-12;
+                outHour = newHour;
+            } else {
+                var ampm = "AM";   
+            }
+            $('.ampm-indicator').text(ampm);
+            
+            if(outHour<10) {
+                $('.time-hour').text("0" + outHour); // leading 0
+            } else {
+                $('.time-hour').text(outHour);
+            }
+            
+            var outMin = now.getMinutes();
+            if(outMin<10) {
+                $('.time-min').text("0"+outMin); // leading 0
+            } else {
+                $('.time-min').text(outMin);
+            }
+            
+            //  var outSec = now.getSeconds();
+            //  if(outSec<10) {
+                 // document.getElementById('SecDiv').innerHTML="0"+outSec;
+            //  } else {
+                //  document.getElementById('SecDiv').innerHTML=outSec;
+            //  }
+              }
+    
+          myDate();
+          setInterval(function() {
+              myDate();
+          }, 1000);
+        });
