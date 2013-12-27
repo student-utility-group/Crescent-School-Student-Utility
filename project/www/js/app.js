d7879ddc8579e51a64d6a8156243d02911178bb1
Libbux <j@thelibbster.com>
2013-12-26 23:38:51 -0500

    [20412]
    Cleaned up files and prepared home page for actual operation.

--- project/www/js/app.js
+++ project/www/js/app.js
@@ -1,72 +1,63 @@
-$(document).ready(function(){
-        // Attach the FastClick library to the .click event
-        // FastClick.js
-        // https://github.com/ftlabs/fastclick
-        $(function () {
-            FastClick.attach(document.body);
-        });
-
-         
-        
-        // Main login-page event handling for the login form submission
-//        $('#login-button').click(function () {
-//            $('.login-page').slideUp();
-//            $('.main-page').slideDown();
-//        });
-        
-            $('.main-page').hide();
-        
-        $(function () {
-            FastClick.attach(document.body);
-        });
-        
-        $('#login-button').click(function () {
-            $('.login-page').slideUp();
-            $('.main-page').slideDown();
-        });
-        
-        $('.footer-build').click(function () {
-            $('.main-page').slideUp();
-            $('.login-page').slideDown();
-        });
+$(document).ready(function () {
+        // Hide the main page
+    $('.main-page').hide();
+    // Attach the FastClick library to the .click event
+    // FastClick.js
+    // https://github.com/ftlabs/fastclick
+    $(function () {
+        FastClick.attach(document.body);
+    });
     
-        function myDate(){
-            
-            var now = new Date();
-            var outHour = now.getHours();
-            
-            if(outHour >12) {
-                var ampm = "PM";
-                newHour = outHour-12;
-                outHour = newHour;
-            } else {
-                var ampm = "AM";   
-            }
-            $('.ampm-indicator').text(ampm);
-            
-            if(outHour<10) {
-                $('.time-hour').text("0" + outHour); // leading 0
-            } else {
-                $('.time-hour').text(outHour);
-            }
-            
-            var outMin = now.getMinutes();
-            if(outMin<10) {
-                $('.time-min').text("0"+outMin); // leading 0
-            } else {
-                $('.time-min').text(outMin);
-            }
-            
-            //  var outSec = now.getSeconds();
-            //  if(outSec<10) {
-                 // document.getElementById('SecDiv').innerHTML="0"+outSec;
-            //  } else {
-                //  document.getElementById('SecDiv').innerHTML=outSec;
-            //  }
-              }
     
+    $(function () {
+        FastClick.attach(document.body);
+    });
+    
+    $('#login-button').click(function () {
+        $('.login-page').slideUp();
+        $('.main-page').slideDown();
+    });
+    
+    $('.footer-build').click(function () {
+        $('.main-page').slideUp();
+        $('.login-page').slideDown();
+    }); 
+    function myDate() {
+    
+        var now = new Date();
+        var outHour = now.getHours();
+        var ampm;
+        if (outHour >= 12) {
+            ampm = "PM";
+            var newHour = outHour - 12;
+            outHour = newHour;
+        } else {
+            ampm = "AM";   
+        }
+        $('.ampm-indicator').text(ampm);
+        
+        if (outHour < 10) {
+            $('.time-hour').text("0" + outHour); // leading 0
+        } else {
+            $('.time-hour').text(outHour);
+        }
+        
+        var outMin = now.getMinutes();
+        if (outMin < 10) {
+            $('.time-min').text("0"+outMin); // leading 0
+        } else {
+            $('.time-min').text(outMin);
+        }
+        
+        //  var outSec = now.getSeconds();
+        //  if(outSec<10) {
+        // document.getElementById('SecDiv').innerHTML="0"+outSec;
+        //  } else {
+        //  document.getElementById('SecDiv').innerHTML=outSec;
+        //  }
+    }
+    myDate();
+    setInterval(function() {
           myDate();
-          setInterval(function() {
-              myDate();
-          }, 1000);
-        });
+    }, 1000);
+});
